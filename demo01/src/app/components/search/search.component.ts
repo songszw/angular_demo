5dd24ff7d34c1e14e7b6821de2b7498ccae41a1f
songszw <songszw@gmail.com>
2019-03-28 17:55:34 +0800

    angular双向数据绑定demo

--- demo01/src/app/components/search/search.component.ts
+++ demo01/src/app/components/search/search.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.styl']
 })
 export class SearchComponent implements OnInit {
-
+  public searchVal:string=''
+  public historyList:any[]=[]
   constructor() { }
-
   ngOnInit() {
   }
+  ngSearchClick(){
+    if(this.searchVal){
+      if(this.historyList.indexOf(this.searchVal)==-1){
+        this.historyList.push(this.searchVal)
+      }
+      this.searchVal=''
+    }
+  }
+  ngDeleteClick(key){
+    this.historyList.splice(key,1)
+  }
+  ngSearchAgain(item){
+    this.searchVal = item
+  }
+
 
 }
