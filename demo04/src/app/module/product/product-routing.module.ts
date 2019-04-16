b7feb0f0d68f086ab0c4df1a44f3b12e65d6cba4
songszw <songszw@gmail.com>
2019-04-16 17:51:14 +0800

    angular路由模块懒加载

--- demo04/src/app/module/product/product-routing.module.ts
+++ demo04/src/app/module/product/product-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { ProductComponent } from './product.component'
-
+import { CartComponent } from './components/cart/cart.component'
+import { PcontentComponent } from './components/pcontent/pcontent.component'
+import { PlistComponent } from './components/plist/plist.component'
 const routes: Routes = [
   {
     path: '',
-    component: ProductComponent
+    component: ProductComponent,
+    children: [
+      {
+        path: 'plist',
+        component: PlistComponent
+      },
+      {
+        path: 'cart',
+        component: CartComponent
+      }
+    ]
+  },
+  {
+    path: 'pcontent',
+    component: PcontentComponent
   }
 ]
 
--- demo04/src/app/module/product/product.module.ts
+++ demo04/src/app/module/product/product.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { PcontentComponent } from './components/pcontent/pcontent.component';
+import { PlistComponent } from './components/plist/plist.component';
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, CartComponent, PcontentComponent, PlistComponent],
   imports: [
     CommonModule,
     ProductRoutingModule
