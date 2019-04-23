eb028eefa7cb4bd22f7742c15b4be4bc33858e2d
songszw <songszw@gmail.com>
2019-04-23 09:34:26 +0800

    angular小栗子todolist小demo

--- demo05/src/app/pages/setup/setup.component.ts
+++ demo05/src/app/pages/setup/setup.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core'
-
+import { Component, HostBinding, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 import { LocalStorageService } from '../../services/localStorage/local-storage.service'
 import {
   USERNAME,
@@ -7,20 +7,24 @@ import {
   START_USING_DATE
 } from '../../services/localStorage/local-storage.namespace'
 import { getTodayTime } from '../../../utils/time'
+import { setupTransition } from '../setup/setup.animation'
 
 @Component({
   selector: 'app-setup',
   templateUrl: './setup.component.html',
-  styleUrls: ['./setup.component.styl']
+  styleUrls: ['./setup.component.styl'],
+  animations: [setupTransition]
 })
 export class SetupComponent implements OnInit {
+  @HostBinding('@setupTransition') state = 'activated'
   public username: string
-  constructor(private store: LocalStorageService) {}
+  constructor(private store: LocalStorageService, private router: Router) {}
   ngOnInit() {}
 
   completeSetup(): void {
     this.store.set(INIT_FLAG, true)
     this.store.set(START_USING_DATE, getTodayTime())
     this.store.set(USERNAME, this.username)
+    this.router.navigateByUrl('main')
   }
 }
