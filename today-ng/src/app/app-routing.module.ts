ef97f41f39a5edb4f04821d8317d681db4ee7e53
songszw <songszw@gmail.com>
2019-04-25 17:18:14 +0800

    添加main页面

--- today-ng/src/app/app-routing.module.ts
+++ today-ng/src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { SetupComponent } from './pages/setup/setup.component'
+import { MainComponent } from './pages/main/main.component'
 
 const routes: Routes = [
-  {
-    path: 'setup',
-    component: SetupComponent
-  },
-  {
-    path: '',
-    redirectTo: '/setup',
-    pathMatch: 'full'
-  }
+  { path: 'setup', component: SetupComponent },
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo: '/setup', pathMatch: 'full' }
 ]
 
 @NgModule({
--- today-ng/src/app/app.module.ts
+++ today-ng/src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
+import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd'
@@ -10,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { registerLocaleData } from '@angular/common'
 import zh from '@angular/common/locales/zh'
 import { SetupModule } from './pages/setup/setup.module'
+import { MainModule } from './pages/main/main.module'
 
 registerLocaleData(zh)
 
@@ -22,7 +22,8 @@ registerLocaleData(zh)
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    SetupModule
+    SetupModule,
+    MainModule
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
