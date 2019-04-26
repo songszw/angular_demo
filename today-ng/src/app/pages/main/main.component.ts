f60e4bffcc147f6038a6443cb56fcf0f030754bb
songszw <songszw@gmail.com>
2019-04-26 10:34:53 +0800

    添加main页面

--- today-ng/src/app/pages/main/main.component.ts
+++ today-ng/src/app/pages/main/main.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core'
 })
 export class MainComponent implements OnInit {
   constructor() {}
+  isCollapsed = false
 
   ngOnInit() {}
+  nzCollapsed() {}
 }
--- today-ng/src/app/pages/main/main.module.ts
+++ today-ng/src/app/pages/main/main.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MainComponent } from './main.component'
 import { FormsModule } from '@angular/forms'
+import { NgZorroAntdModule } from 'ng-zorro-antd'
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, NgZorroAntdModule]
 })
 export class MainModule {}
