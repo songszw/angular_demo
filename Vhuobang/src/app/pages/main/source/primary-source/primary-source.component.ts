646f840c8a539556df98690fbe6d344eeb8fc5d9
songszw <songszw@gmail.com>
2019-05-21 17:42:27 +0800

    完成高级货源搜索框

--- Vhuobang/src/app/pages/main/source/primary-source/primary-source.component.ts
+++ Vhuobang/src/app/pages/main/source/primary-source/primary-source.component.ts
@@ -41,8 +41,7 @@ export class PrimarySourceComponent implements OnInit {
           children: [
             {
               value: 'zhonghuamen',
-              label: 'Zhong Hua Men',
-              isLeaf: true
+              label: 'Zhong Hua Men'
             }
           ]
         }
@@ -58,13 +57,15 @@ export class PrimarySourceComponent implements OnInit {
   pageSize: number = 20
   total: number = 1
   listOfData: any = []
+  loading = true
 
-  constructor(
-    private primarySource: SourceService,
-    private areaData: AreaDataService
-  ) {}
+  constructor(private primarySource: SourceService) {}
 
-  primaryData(): void {
+  primaryData(reset: boolean = false): void {
+    if (reset) {
+      this.pageIndex = 1
+    }
+    this.loading = true
     // 获取初级货源
     console.log('aaa')
     this.primarySource
@@ -72,6 +73,7 @@ export class PrimarySourceComponent implements OnInit {
       .subscribe((data: any) => {
         if (data.code === 0) {
           console.log(data)
+          this.loading = false
           let desc = data.data.pagination
           this.pageIndex = desc.pageNum
           this.pageSize = desc.pageSize
@@ -111,11 +113,15 @@ export class PrimarySourceComponent implements OnInit {
     this.primaryData()
   }
   onChanges(values: any): void {
-    console.log('aaabbb', values)
+    // console.log('aaabbb', values)
   }
 
   loadData(node: any, index: number): PromiseLike<any> {
-    console.log('aaa', index)
+    // console.log('aaa', window.location.origin)
+
+    // let provinceUrl: string =
+    //   'http://localhost:4200/test' + '/address/province.do?t=' + Date.now()
+    // console.log(provinceUrl)
 
     // this.areaData.getProviceData().subscribe((data: any) => {
     //   console.log('data:', data)
@@ -123,15 +129,61 @@ export class PrimarySourceComponent implements OnInit {
     return new Promise(resolve => {
       setTimeout(() => {
         if (index < 0) {
-          // console.log(this)
-          // this.areaData.getProviceData().subscribe((data: any) => {
-          //   console.log(data)
-          // })
-          // if index less than 0 it is root node
-          // node.children = provinces
+          node.children = [
+            {
+              value: 'zhejiang',
+              label: 'Zhejiang'
+            },
+            {
+              value: 'jiangsu',
+              label: 'Jiangsu'
+            }
+          ]
         } else if (index === 0) {
-          // node.children = cities[node.value]
+          console.log(index)
+          // const cities: {
+          //   [key: string]: Array<{
+          //     value: string
+          //     label: string
+          //     isLeaf?: boolean
+          //   }>
+          // } = {
+          //   zhejiang:
+          //   ,
+          //   jiangsu: [
+          //     {
+          //       value: 'nanjing',
+          //       label: 'Nanjing'
+          //     }
+          //   ]
+          // }
+          // console.log(node.value)
+
+          node.children = [
+            {
+              value: 'hangzhou',
+              label: 'Hangzhou'
+            },
+            {
+              value: 'ningbo',
+              label: 'Ningbo',
+              isLeaf: true
+            }
+          ]
         } else {
+          // console.log(index)
+          node.children = [
+            {
+              value: 'hangzhou',
+              label: 'Hangzhou',
+              isLeaf: true
+            },
+            {
+              value: 'ningbo',
+              label: 'Ningbo',
+              isLeaf: true
+            }
+          ]
           // node.children = scenicspots[node.value]
         }
         resolve()
