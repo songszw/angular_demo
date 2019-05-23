f6a372d529fe2c3d7e3bd5da9b6b0fdb5b33f748
songszw <songszw@gmail.com>
2019-05-23 14:12:08 +0800

    完成高级货源查询筛选功能

--- Vhuobang/src/app/pages/main/source/senior-source/senior-source.component.ts
+++ Vhuobang/src/app/pages/main/source/senior-source/senior-source.component.ts
@@ -9,24 +9,74 @@ import { SourceService } from 'src/app/services/source/source.service'
 })
 export class SeniorSourceComponent implements OnInit {
   material: string = ''
+  startDate = null
   startValue: any = ''
+  endDate = null
   endValue: any = ''
-  goodsState: string = ''
   pageIndex: number = 1
   pageSize: number = 3
   total = 1
   seniorData = []
+  sourceStatus: any = [
+    {
+      value: '',
+      label: '全部货源'
+    },
+    {
+      value: '0',
+      label: '待匹配'
+    },
+    {
+      value: '1',
+      label: '匹配中'
+    },
+    {
+      value: '2',
+      label: '匹配完成'
+    },
+    {
+      value: '3',
+      label: '已失效'
+    }
+  ]
+  goodsState: string = ''
 
   constructor(private getData: SourceService) {}
 
   ngOnInit() {
     this.getSeniorList()
   }
+  getMenuItem(e: any): void {
+    // console.log(e)
+    this.goodsState = e
+    this.material = ''
+    this.startValue = ''
+    this.endValue = ''
+    this.startDate = null
+    this.endDate = null
+    this.pageIndex = 1
+    this.getSeniorList()
+  }
+
+  startDateChange(result: Date): void {
+    if (result) {
+      this.startValue = formatDate('yyyy-MM-dd', result)
+    } else {
+      this.startValue = ''
+    }
+  }
+  endDateChange(result: Date): void {
+    if (result) {
+      this.endValue = formatDate('yyyy-MM-dd', result)
+    } else {
+      this.endValue = ''
+    }
+  }
 
   //查询
   handleSearchClick() {
-    this.startValue = formatDate('yyyy-MM-dd', this.startValue)
-    this.endValue = formatDate('yyyy-MM-dd', this.endValue)
+    this.getSeniorList()
+    console.log(this.startValue, this.endValue, this.material, this.goodsState)
   }
   // 获取数据
   getSeniorList() {
